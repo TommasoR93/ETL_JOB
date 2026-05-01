9b1e5bd4910e7d9197d4634c9a211c39b6bb68cc
Tommaso Ruffini <tommaso.ruffini93@gmail.com>
2026-05-01 11:51:32 +0200

    Ingestion_complete

--- src/ingestion.py
+++ src/ingestion.py
@@ -6,19 +6,21 @@ import os
 from config.config import load_config
 import time
 import pandas as pd
+import json
 from utils.logging import create_log
+from pathlib import Path
 
 load_dotenv()
 logger = create_log()
 
 conf = load_config()["API"]
-BASE_URL = f"http://api.adzuna.com/v1/api/jobs/{conf.get("country", "")}/search/{conf.get("page", "")}"
+# BASE_URL = f"http://api.adzuna.com/v1/api/jobs/{conf.get("country", "")}/search/{conf.get("page", "")}"
+BASE_URL = f"http://api.adzuna.com/v1/api/jobs/{conf.get("country", "")}/search"
 params = {
     "app_id" : os.getenv("APP_ID"),
     "app_key" : os.getenv("APP_KEY"),
     "results_per_page" : conf.get("results_per_page", "")
 }
-last_request_time = 0
 
 def create_session():
     retry_strategy = Retry(
@@ -33,7 +35,7 @@ def create_session():
     session.mount("http://", adapter)
     return session
 
-def rate_limit(last_request_time, interval = 6 ):   
+def rate_limit(last_request_time, interval = 0.3 ):   
     now = time.time()
     elapsed = now - last_request_time
     if elapsed < interval:
@@ -46,37 +48,37 @@ def make_request(session, url, last_request_time, params):
     return response, last_request_time
 
 def pagination(session):
-    all_results = []
     page = 1
     last_request_time = 0
     while True:
-        response, last_request_time = make_request(session, BASE_URL, last_request_time, params)
+        url = f"{BASE_URL}/{page}"
+        response, last_request_time = make_request(session, url, last_request_time, params)
         if response.status_code != 200:
             logger.info(f"Error {response.status_code}")       
-        data = response.json()
-        result = data.get("results", "")
-        if not result:
+        data = response.json()        
+        result = data.get("results", [])
+        logger.info(f"Fetching page {page}")
+        if page > 10:
             break
-        all_results.extend(result)
+        save_json(result, page)
         page += 1
-    return all_results
+    
 
-def create_df(session):
-    result = pagination(session)
-    if not result:
-        logger.info(f"{result} empty")
-    else:
-        df = pd.json_normalize(result)
-    return df
+def save_json(all_results, page):
+    path = Path(__file__).resolve().parents[1] / "data"
+    path.mkdir(parents=True, exist_ok=True)
+    file_path = path / f"page_{page}.json"
+
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(all_results, f, ensure_ascii=False, indent=2)
+    
 
 def main():
     session = create_session()
     logger.info(f"{session} created")
-    all_result = pagination(session)
-    logger.info(f"{all_result} paginated")
-    df = create_df(session)
-    logger.info(f"{df} created")
-    return df
+    pagination(session)
+    logger.info("Pagination and saving completed")   
+    
 
 if __name__ == "__main__":
     main()
--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -1,4 +1,4 @@
-from src.ingestion import create_session, rate_limit, make_request, pagination, create_df
+from src.ingestion import create_session, rate_limit, make_request, pagination, save_json
 from requests.adapters import HTTPAdapter
 from unittest.mock import patch, MagicMock
 
@@ -28,48 +28,5 @@ def test_make_requests(mock_rate_limit):
     assert response == fake_response
     assert last_request_time == 123
 
-@patch("src.ingestion.make_request")
-def test_pagination(mock_make_request):
 
-    session = MagicMock()
-    #resp_1
-    resp_1 = MagicMock()
-    resp_1.status_code = 200
-    resp_1.json.return_value = {"results": [{"name": "file1_name.csv"}]}
-    
-    #reponse 2
-    resp_2 = MagicMock()
-    resp_2.status_code = 200
-    resp_2.json.return_value = {"results": []}
 
-    mock_make_request.side_effect = [
-        (resp_1, 1), 
-        (resp_2, 2)
-    ]
-
-    files = pagination(session)
-    assert files == [{"name":"file1_name.csv"}]
-
-@patch("src.ingestion.pagination")
-def test_create_df_columns(mock_pagination):
-    mock_pagination.return_value = [
-        {"name": "file1.csv"},
-        {"name": "file2.csv"},
-    ]
-
-    session = MagicMock()
-    df = create_df(session)
-    assert list(df.columns) == ["name"]
-    assert len(df) == 2
-
-@patch("src.ingestion.pagination")
-def test_create_df_values(mock_pagination):
-    mock_pagination.return_value = [
-        {"name": "file1.csv"},
-        {"name": "file2.csv"},
-    ]
-
-    session = MagicMock()
-    df = create_df(session)
-    assert df["name"].tolist() == ["file1.csv", "file2.csv"]
-    
